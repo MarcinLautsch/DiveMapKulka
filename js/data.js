27b3f7382ed4f5cf459751e866f352ac1bae9512
Marcin Lautsch <mldemigod@gmail.com>
2022-11-15 11:26:31 +0100

    work_with_JS

---
+++ js/data.js
@@ -0,0 +1,62 @@
+const mapa = [
+	{
+		id: 1,
+		image: './src/lodka.jpg',
+		object: 'Platforma',
+		depth: '6m',
+		timeFromStart: '6min',
+		danger: 'brak',
+		position: {
+			left: 10, //zmina na number w celu łatwijszego pozycjonowania
+			top: 15, //zmina na number w celu łatwijszego pozycjonowania
+		},
+	},
+	{
+		id: 2,
+		image: './src/ramie.jpg',
+		object: 'WÓZKI KOPALNIANE 2 SZT',
+		depth: '8m',
+		timeFromStart: '12min',
+		danger: 'brak',
+		position: {
+			left: 50,
+			top: 25,
+		},
+	},
+	{
+		id: 3,
+		image: './src/ramie.jpg',
+		object: 'WÓZKI KOPALNIANE 2 SZT',
+		depth: '8m',
+		timeFromStart: '12min',
+		danger: 'brak',
+		position: {
+			left: 75,
+			top: 55,
+		},
+	},
+	{
+		id: 4,
+		image: './src/ramie.jpg',
+		object: 'WÓZKI KOPALNIANE 2 SZT',
+		depth: '8m',
+		timeFromStart: '12min',
+		danger: 'brak',
+		position: {
+			left: 75,
+			top: 55,
+		},
+	},
+	{
+		id: 5,
+		image: './src/ramie.jpg',
+		object: 'WÓZKI KOPALNIANE 2 SZT',
+		depth: '8m',
+		timeFromStart: '12min',
+		danger: 'brak',
+		position: {
+			left: 75,
+			top: 55,
+		},
+	},
+]
---
+++ js/mapa.js
@@ -0,0 +1,68 @@
+// generuje claego htmla renderuje z tablicy
+
+function generateData() {
+	const container = document.getElementById('map')
+
+	mapa.forEach(point => {
+		container.innerHTML += `
+		<button id="button-${point.id}" class="point" type="button" style="left: ${point.position.left}%; top: ${
+			point.position.top
+		}%">+</button>
+	
+		<div
+			id="popap-container-button-${point.id}"
+			class="popap-container"
+			style="
+				left: ${point.position.left}%;   
+				top: ${point.position.top - 9}%
+			"
+		>
+			<div class="popap-container__shadow"></div>
+			<div class="popap-container__content">
+				<div class='info'>
+					<img class="img-info" src="${point.image}">
+					<p class="diveInfo">
+						<span>
+							<ol class="infoPlace">
+								<li>Obiekt:<span> ${point.object}</span></li>
+								<li>Głębokość: <span> ${point.depth}</span></li>
+								<li>Czas z punktu Start:<span> ${point.timeFromStart}</span></li>
+								<li>Zagrożenia: <span> ${point.danger}</span></li>
+							</ol>
+						</span>
+					</p>
+				</div>
+			</div>
+		</div>
+		`
+	})
+}
+
+function generateLegend() {
+	const container = document.getElementById('legend-list')
+	mapa.forEach(point => {
+		container.innerHTML += `
+		<li id='list-element-${point.id}'><a class="btn-photo"><span>${point.object} </span> ${point.depth}</li></a>
+	`
+	})
+}
+
+generateData()
+generateLegend()
+
+// funkcja ktora wywoluje popap
+
+function handleOnClick(event) {
+	const eventTargetId = event.target.id
+	const popapContainer = document.getElementById(`popap-container-${eventTargetId}`)
+	popapContainer.classList.toggle('popap-container--is-visible')
+}
+
+// czeka na załadowanie strony oraz javascript ponizej zapis i przypisuje EventHandler> OnClick do button
+
+window.addEventListener('load', function () {
+	mapa.forEach(point => {
+		const button = document.getElementById(`button-${point.id}`)
+		button.addEventListener('click', handleOnClick)
+	})
+})
